e668c83e9a9291f015bd53464c08f6f33a922784
Rahilsiddique <siddiquerahil19@gmail.com>
2023-01-20 23:06:15 +0530

    sales page
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- components/often-used/ProductCard.jsx
+++ components/often-used/ProductCard.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 const ProductCard = () => {
+  const [heart, setHeart] = useState(true);
   return (
-    <div className="bg-gray-100 bg-pink-100 shadow-xl card w-96">
+    <div className="transition duration-200 bg-gray-100 hover:shadow-xl card w-96">
+      <div className="absolute badge top-4 left-4">50% off</div>
+      <span
+        className="absolute cursor-pointer right-4 top-4"
+        onClick={() => setHeart(p => !p)}
+      >
+        {heart ? <AiOutlineHeart size={30} /> : <AiFillHeart size={30} />}
+      </span>
       <figure>
         <img src="/adidasbrand.png" alt="Shoes" layout="fill" />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div className="cursor-pointer card-body">
+        <div className="flex items-center justify-between">
+          <h2 className="card-title">Jordan dunks</h2>
+          <div className="flex items-center gap-2">
+            <span className="text-lg">₹1,200</span>
+            <span className="text-sm text-gray-500 line-through">₹2,400</span>
+          </div>
+        </div>
+        <div>If a dog chews shoes whose shoes does he choose?</div>
         <div className="justify-end card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-[#0782F4]">Buy Now</button>
         </div>
       </div>
     </div>
--- pages/Sales.js
+++ pages/Sales.js
@@ -1,20 +1,42 @@
 import React from "react";
 import ProductCard from "../components/often-used/ProductCard";
 function Sales() {
-  // sales page work
-  return (
-    <div className="grid justify-center gap-4 m-3 md:grid-cols-3">
-      <div className="flex justify-center">
-        <ProductCard />
-      </div>
-      <div className="flex justify-center">
-        <ProductCard />
-      </div>
-      <div className="flex justify-center">
-        <ProductCard />
-      </div>
-    </div>
-  );
+    // sales page work
+    return (
+        <>
+            <div className="flex items-center justify-between mx-3">
+                <div className="text-xl font-bold">Divenco Shoes</div>
+                <div className="flex gap-4 itesm-center">
+                    <div className="form-control">
+                        <label className="flex gap-2 cursor-pointer label">
+                            <span className="text-base label-text">Show filters</span>
+                            <input type="checkbox" className="toggle toggle-primary" />
+                        </label>
+                    </div>
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="m-1 btn-sm btn">Sort by</label>
+                        <ul tabIndex={0} className="p-2 shadow dropdown-content menu bg-base-100 rounded-box w-52">
+                            <div className="form-control">
+                                <label className="cursor-pointer label">
+                                    <span className="label-text">High to Low</span>
+                                    <input type="radio" name="radio-10" className="radio checked:bg-red-500" checked />
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="cursor-pointer label">
+                                    <span className="label-text">Low to High</span>
+                                    <input type="radio" name="radio-10" className="radio checked:bg-blue-500" checked />
+                                </label>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="grid gap-4 m-3 justify-items-center xl:grid-cols-3 xl:mx-24 lg:grid-cols-2">
+                {[...Array(30)].map(i => <ProductCard key={i} />)}
+            </div>
+        </>
+    );
 }
 
 export default Sales;
