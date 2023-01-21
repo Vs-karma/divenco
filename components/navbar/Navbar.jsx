c737fc31cc7ed9d9a8e4ca3be4a15067eee158fd
Rahilsiddique <siddiquerahil19@gmail.com>
2023-01-21 22:34:53 +0530

    login / signup page started
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -3,9 +3,14 @@ import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
 import { RxAvatar } from "react-icons/rx";
+import { useState } from "react";
+import Login from "../auth/Login";
 
 function Navbar() {
+  const [modal, setModal] = useState(false);
+  console.log(modal);
   return (
+    <>
     <div className="z-10 flex justify-between px-2 py-4 bg-white md:w-full">
       <div className="">
         <Image
@@ -14,7 +19,7 @@ function Navbar() {
           height={50}
           alt="logo"
           className="w-24 h-auto"
-        />
+          />
       </div>
       <div className="md:text-3xl text-xl md:w-[100%] md:justify-center md:flex md:gap-24 font-bold items-center hidden">
         <Link href="/">Home</Link>
@@ -23,17 +28,19 @@ function Navbar() {
         <Link href="/Sales">Sale</Link>
       </div>
       <div className="flex items-center gap-3">
-        <div>
+        <div className="cursor-pointer">
           <AiOutlineShoppingCart size={30} />
         </div>
-        <div>
+        <div className="cursor-pointer">
           <AiOutlineHeart size={30} />
         </div>
-        <div>
+        <div className="cursor-pointer" onClick={() => setModal(p => !p)}>
           <RxAvatar size={30} />
         </div>
-      </div>
+      </div>  
     </div>
+    {modal ? <Login /> : null}
+    </>
   );
 }
 
