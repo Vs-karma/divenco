a073dae9baafdbda348675247cd048caf3519da2
Rahilsiddique <siddiquerahil19@gmail.com>
2023-01-23 18:09:36 +0530

    changes
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- components/auth/Login.jsx
+++ components/auth/Login.jsx
@@ -5,21 +5,3 @@ const Login = () => {
 };
 
 export default Login;
-
-{
-  /* <label className="absolute btn btn-sm btn-circle right-2 top-2">
-          ✕
-        </label>
-        <label>User Name</label>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="w-full max-w-xs input input-bordered"
-        />
-        <label>Password</label>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="w-full max-w-xs input input-bordered"
-        /> */
-}
