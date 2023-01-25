d53f9b357b07b70a985f8447a231fed4444f8ee3
Samridhi-Sinha <74322386+Samridhi-Sinha@users.noreply.github.com>
2023-01-25 16:59:30 +0530

    Sales page and footer
    Signed-off-by: Samridhi-Sinha <74322386+Samridhi-Sinha@users.noreply.github.com>

--- pages/ProductPage.jsx
+++ pages/ProductPage.jsx
@@ -7,9 +7,11 @@ import pp from "../styles/productpage.module.css";
 function ProductPage() {
   return (
     <div className={pp.mainFrame}>
+      <div className="p-5 text-2xl font-semibold text-gray-500">
       <Link href="#">
         <a className="goBackLink">Go Back</a>
       </Link>
+      </div>
       <div className="flex flex-wrap justify-between">
         <ProductCarousel />
         <ProductPageDetail />
